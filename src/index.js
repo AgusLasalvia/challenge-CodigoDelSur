8d0f3f0aa001757fa3392f8d99ecb5f059133e86
AgusLasalvia <aguslblumenfeld@gmail.com>
2023-12-01 12:39:32 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 //Node JS modules import
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const fs = require('fs');
-const { stringify } = require('querystring');
-const { database } = require('./db/sql3')
 
 // Import Routes
+const registro = require('./routes/registro');
+const auth = require('./routes/auth');
 
 
 // Decalre port variable
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+// Load the users data at the init
+// and save it as a list 
+module.users = users = JSON.parse(fs.readFileSync('./users.txt', 'utf8').toString());
+
+app.get('/', (req, res) => {
+	res.json({response:'success'})
+})
 
 // Express configuration
-app.use(express.json()); // Let express use json
+app.use(cors())
+app.use(bodyParser.json()); // Let express use json
+app.use(express.urlencoded({extended:true})); // Let express use urlencoded
 
+
+// Routes use express
+app.use('/registration',registro);
+app.use('/auth',auth)
 
 
 
 // Express start up listening on port given by environment
-app.listen(PORT,()=>{`http://localhost:${PORT}`})
-
+app.listen(PORT,()=>{console.log(`http://localhost:${PORT}`)});
 
 
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,23 @@
 const { Router } = require('express');
+const {users} = require('../index')
 const route = Router();
 
-route.post('/login', (req,res) => {
-    let { email, password } = req.body;
-    
+route.post('/login/:email', (req,res) => {
+    let email = req.params.email
+	console.log(email)
+	res.send(email)
+	// if (loginValidation(email, password)) res.send(200).json({response: 'success'});
+
+	// else res.send(404).json({response: 'user not found'})
 })
+
+
+const loginValidation = (email,password) => {
+	for (let j of users){
+		if (j.email === email && j.password === password) return true;
+		return false;
+	}
+};
+
+
+module.exports = route;
