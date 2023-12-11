97577b3886c8cdfbee2f2180df58ee63c813447a
AgusLasalvia <aguslblumenfel@gmail.com>
2023-12-11 14:38:53 -0300

    .

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -5,15 +5,9 @@ const { login } = require('../controllers/userController');
 const { Router } = require('express');
 const route = Router();
 
-//--------------------------------------//
-// Endpoints and Methods
-//--------------------------------------//
-
 // POST endpoint for auth URL
 route.post('/login', (req, res) => {
    login(req, res);
 });
-
-
 
 module.exports = route;
